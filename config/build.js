cfa5c8dec3989fb649b981cd6a3c4d8c6078f9c8
Jing Dong <jing.dong@activars.com>
2012-06-04 13:59:52 +0100

    experiment file structure

---
+++ config/build.js
@@ -0,0 +1,19 @@
+({
+  // The app directory is the repository root
+  appDir: "../",
+  baseUrl: "app/",
+  dir: "../build",
+
+  // Comment out the optimize line if you want
+  optimize: "none",
+
+  paths: {
+  },
+
+  modules: [
+    //Optimize the application files.
+    {
+      name: "main"
+    }
+  ]
+})
